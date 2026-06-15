0dae38f798a4cb4c2dce338d868ca1150654cd2d
AlvaroCoe <alvarodlcoelho@gmail.com>
2026-06-15 20:54:21 -0300

    Atualização do Corpo clínico mais imagens

--- atividade-hospital/src/pages/CorpoClinico/CorpoClinico.jsx
+++ atividade-hospital/src/pages/CorpoClinico/CorpoClinico.jsx
@@ -1,13 +1,37 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
+import anaSilva from "../../assets/medicos/ana-silva.jpg";
+import alvaroCoelho from "../../assets/medicos/alvaro-coelho.jpg";
+import julianaSouza from "../../assets/medicos/juliana-souza.jpg";
+import daniloSena from "../../assets/medicos/danilo-sena.jpg";
+
+// Adicione esta linha bem aqui:
+import "./corpo.css";
+
 function CorpoClinico() {
 
   const medicos = [
-    ["Dra. Ana Silva","Cardiologista"],
-    ["Dr. Rafael Costa","Pediatra"],
-    ["Dra. Juliana Rocha","Dermatologista"],
-    ["Dr. Felipe Santos","Ortopedista"]
+    {
+      nome: "Dra. Ana Silva",
+      especialidade: "Cardiologista",
+      foto: anaSilva
+    },
+    {
+      nome: "Dr. Alvaro Coelho",
+      especialidade: "Pediatra",
+      foto: alvaroCoelho
+    },
+    {
+      nome: "Dra. Juliana Souza",
+      especialidade: "Dermatologista",
+      foto: julianaSouza
+    },
+    {
+      nome: "Dr. Danilo Sena",
+      especialidade: "Ortopedista",
+      foto: daniloSena
+    }
   ];
 
   return (
@@ -21,10 +45,18 @@ function CorpoClinico() {
 
           <div className="cards">
 
-            {medicos.map((m,index)=>(
-              <div className="card" key={index}>
-                <h3>{m[0]}</h3>
-                <p>{m[1]}</p>
+            {medicos.map((medico, index) => (
+              <div className="card-medico" key={index}>
+
+                <img
+                  src={medico.foto}
+                  alt={medico.nome}
+                  className="foto-medico"
+                />
+
+                <h3>{medico.nome}</h3>
+                <p>{medico.especialidade}</p>
+
               </div>
             ))}
 
